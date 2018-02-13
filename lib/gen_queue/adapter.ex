58ff5b68a8adcff5da8bf74c90aaad3750992f6f
Nick Sweeting <nsweeting@gmail.com>
2018-02-12 20:26:49 -0500

    Update adapter.ex

--- lib/gen_queue/adapter.ex
+++ lib/gen_queue/adapter.ex
@@ -1,23 +1,29 @@
 defmodule GenQueue.Adapter do
-  @callback handle_push(atom, binary, any) :: {:ok, any} | {:error, any}
+  @callback handle_push(GenQueue.t(), GenQueue.queue(), any) :: {:ok, any} | {:error, any}
 
-  @callback handle_pop(atom, binary) :: {:ok, any} | {:error, any}
+  @callback handle_pop(GenQueue.t(), GenQueue.queue()) :: {:ok, any} | {:error, any}
 
-  @callback handle_flush(atom, binary) :: {:ok, integer} | {:error, any}
+  @callback handle_flush(GenQueue.t(), GenQueue.queue()) :: {:ok, integer} | {:error, any}
+  
+  @callback handle_size(GenQueue.t(), GenQueue.queue()) :: {:ok, integer} | {:error, any}
 
   defmacro __using__(_) do
     quote location: :keep do
       @behaviour GenQueue.Adapter
 
-      def handle_push(_enqueuer, _queue, _item) do
+      def handle_push(_gen_queue, _queue, _item) do
         {:error, :not_implemented}
       end
 
-      def handle_pop(_enqueuer, _queue) do
+      def handle_pop(_gen_queue, _queue) do
         {:error, :not_implemented}
       end
 
-      def handle_flush(_enqueuer, _queue) do
+      def handle_flush(_gen_queue, _queue) do
+        {:error, :not_implemented}
+      end
+
+      def handle_size(_gen_queue, _queue) do
         {:error, :not_implemented}
       end
 
