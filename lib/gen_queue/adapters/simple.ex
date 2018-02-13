525ae4fc7d4233876bf84d58c72f57c44b82d1a8
Nick Sweeting <nsweeting@gmail.com>
2018-02-12 20:18:10 -0500

    Update simple.ex

--- lib/gen_queue/adapters/simple.ex
+++ lib/gen_queue/adapters/simple.ex
@@ -1,19 +1,23 @@
 defmodule GenQueue.Adapters.Simple do
   use GenQueue.Adapter
 
-  def start_link(caller, opts) do
-    GenQueue.Adapters.SimpleServer.start_link(caller, opts)
+  def start_link(gen_queue, opts) do
+    GenQueue.Adapters.SimpleServer.start_link(gen_queue, opts)
   end
 
-  def handle_push(caller, queue, item) do
-    GenServer.call(caller, {:push, queue, item})
+  def handle_push(gen_queue, queue, item) do
+    GenServer.call(gen_queue, {:push, queue, item})
   end
 
-  def handle_pop(caller, queue) do
-    GenServer.call(caller, {:pop, queue})
+  def handle_pop(gen_queue, queue) do
+    GenServer.call(gen_queue, {:pop, queue})
   end
 
-  def handle_flush(caller, queue) do
-    GenServer.call(caller, {:flush, queue})
+  def handle_flush(gen_queue, queue) do
+    GenServer.call(gen_queue, {:flush, queue})
+  end
+  
+  def handle_size(gen_queue, queue) do
+    GenServer.call(gen_queue, {:size, queue})
   end
 end
